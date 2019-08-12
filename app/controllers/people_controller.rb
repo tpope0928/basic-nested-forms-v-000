15a71d8b3dd81c08c8ad1b05991632de4168642b
Thomas Pope <tpope0928@gmail.com>
2019-08-12 22:15:45 +0000

    Automatically backed up by Learn

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -4,18 +4,18 @@ class PeopleController < ApplicationController
     @person.addresses.build(address_type: 'work')
     @person.addresses.build(address_type: 'home')
   end
- 
+
   def create
     person = Person.create(person_params)
     redirect_to people_path
   end
- 
+
   def index
     @people = Person.all
   end
- 
+
   private
- 
+
   def person_params
     params.require(:person).permit(:name)
   end
